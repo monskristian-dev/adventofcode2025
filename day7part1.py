1bb72aa5c97bbba8576bdb1c2fd29db7987e170c
mons <monskristian@propelleraero.com.au>
2025-12-08 08:54:54 +0800

    day 7

---
+++ day7part1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+input_file = "day7.txt"
+
+counter = 0
+
+with open(input_file) as f:
+    lines = [line.strip() for line in f.readlines()]
+
+answer = np.zeros(len(lines[0]))
+
+initial = lines[0].index("S")
+answer[initial] = 1
+
+for i in range(len(lines)-1):
+    for j in range(len(lines[i])):
+        if lines[i+1][j] == "^":
+            if answer[j] == 1:
+                counter += 1
+                answer[j-1] = 1
+                answer[j+1] = 1
+                answer[j] = 0
+
+print (counter)
